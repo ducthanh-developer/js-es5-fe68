c939696e5d2fb94c04df7f348f0f2d9e5fd1be6d
Nguyễn Thành Đức <ducthanh.developer@gmail.com>
2021-06-13 16:38:37 +0700

    end day1

---
+++ es5-day1/main.js
@@ -0,0 +1,16 @@
+console.log('Hello world');
+// This is my comment 
+/**
+ * variable and datatypes:
+ * primitive type: string, number, boolean, undefined, null, bigint, symbol
+ * object
+ */
+
+var fullName = 'welcome to my decktop';
+console.log(typeof fullName);
+var age = 18;
+console.log(typeof age);
+var isStudent = true;
+console.log(typeof isStudent);
+var height;
+console.log(height);
